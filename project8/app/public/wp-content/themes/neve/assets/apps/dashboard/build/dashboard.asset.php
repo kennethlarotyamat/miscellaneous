a8baded4826f0c037948a477b4eb3afe4d734115
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2023-11-14 11:16:08 -0800

    1

--- project8/app/public/wp-content/themes/neve/assets/apps/dashboard/build/dashboard.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('neve-components', 'react', 'wp-api', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b79f8afef810caf89e417f585dff7285');

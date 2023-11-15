50596bb687d610ad424e1ead79e9ba4249daadc4
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2023-11-15 13:02:08 -0800

    1

---
+++ CSS/project8CSS/themes/kadence/functions.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Kadence functions and definitions
+ *
+ * This file must be parseable by PHP 5.2.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package kadence
+ */
+
+define( 'KADENCE_VERSION', '1.1.43' );
+define( 'KADENCE_MINIMUM_WP_VERSION', '5.4' );
+define( 'KADENCE_MINIMUM_PHP_VERSION', '7.2' );
+function my_custom_post_type_events() {
+    $labels = array(
+        'name' => 'Events',
+        'singular_name' => 'Event',
+        'add_new' => 'Add New',
+        'add_new_item' => 'Add New Event',
+        'edit_item' => 'Edit Event',
+        'new_item' => 'New Event',
+        'view_item' => 'View Event',
+        'search_items' => 'Search Events',
+        'not_found' => 'No events found',
+        'not_found_in_trash' => 'No events found in Trash',
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => true,
+        'rewrite' => array('slug' => 'events'),
+        'supports' => array('title', 'editor', 'thumbnail', 'custom-fields', 'excerpt'),
+    );
+
+    register_post_type('events', $args);
+}
+add_action('init', 'my_custom_post_type_events');
+// Bail if requirements are not met.
+if ( version_compare( $GLOBALS['wp_version'], KADENCE_MINIMUM_WP_VERSION, '<' ) || version_compare( phpversion(), KADENCE_MINIMUM_PHP_VERSION, '<' ) ) {
+	require get_template_directory() . '/inc/back-compat.php';
+	return;
+}
+// Include WordPress shims.
+require get_template_directory() . '/inc/wordpress-shims.php';
+
+// Load the `kadence()` entry point function.
+require get_template_directory() . '/inc/class-theme.php';
+
+// Load the `kadence()` entry point function.
+require get_template_directory() . '/inc/functions.php';
+
+// Initialize the theme.
+call_user_func( 'Kadence\kadence' );
